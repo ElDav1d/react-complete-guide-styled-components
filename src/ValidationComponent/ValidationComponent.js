537d234e9ca4d6fad7ac6c1f072234a647188d34
David Vivó Palanca <david.vivo@nexum.com>
2020-11-20 16:29:11 +0100

    Apply dynamic styles to validation component

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -1,8 +1,16 @@
+import styled from 'styled-components';
 import React from 'react';
+
+const StyledH2 = styled.h2`
+  color: ${props => props.textLength < 5 ? 'red' : 'green'};
+  font-weight: ${props => props.textLength < 5 ? 'bold' : 'normal'};
+`;
 
 const validationComponent = (props) => {
   return (
-  <h2>{props.textLength < 5 ? 'Text too short' : 'Text long enough'}</h2>
+  <StyledH2 textLength={props.textLength}>
+    {props.textLength < 5 ? 'Text too short' : 'Text long enough'}
+  </StyledH2>
   )
 };
 
